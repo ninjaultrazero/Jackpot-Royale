971d4dd012b413a7efdf8355b8e40fe3ed1ce9bb
miki131107 <michela.davino1311@gmail.com>
2025-04-28 16:04:50 +0200

    aggiunta

--- python/black_jack/blackJack.py
+++ python/black_jack/blackJack.py
@@ -198,6 +198,7 @@ class BlackjackApp:
             self.player_coins -= self.bet
         
         remove_coins(self.player_coins)
+        ciao=1
         self.end_game()
 
     def show_full_dealer_hand(self):
